b6610dac3f56136b6efaa54499163e1701f70177
Inna Yatsun <yatsun7@gmail.com>
2023-03-15 23:01:48 +0200

    task from ds

---
+++ home_work_1.sql
@@ -0,0 +1,24 @@
+select distinct ship_city
+from orders;
+
+select *
+from orders
+order by ship_country;
+
+select *
+from orders
+order by ship_country;
+
+select c.category_name, avg(unit_price) as avg_price
+from products p join categories c 
+on p.category_id = c.category_id
+group by c.category_name;
+
+select c.category_name, sum(units_in_stock) as max_quantity
+from products p join categories c 
+on p.category_id = c.category_id 
+group by c.category_name
+order by max_quantity desc limit 3;
+
+
+
