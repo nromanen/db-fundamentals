e8e3a1881daed8fd0d7e71726c2e27973a4ba671
Roman Sitko <sitkoroman@gmail.com>
2023-03-20 13:15:26 +0200

    L4 homework

--- L3- AccessingData.sql
+++ L3- AccessingData.sql
@@ -1,21 +1,21 @@
 --Show all info about company_name and address from the customers table.
-select company_name, address from customers 
+select company_name, address from customers; 
 
 --Show all info about the employee with ID 8
-select * from employees where employee_id = 8
+select * from employees where employee_id = 8;
 
 --Show the list of products with the price between 10 and 50.
 select product_id
 , product_name
 , unit_price 
-from products where unit_price between 10 and 50
+from products where unit_price between 10 and 50;
 
 --Show the list of products which names start form ‘N’ and price is greater than 50.
 select product_id
 , product_name
 , unit_price 
 from products 
-where product_name like 'N%' and unit_price > 50
+where product_name like 'N%' and unit_price > 50;
 
 --Show the list of first, last names and ages of the employees 
 --whose age is greater than average age of all employees. 
@@ -30,7 +30,7 @@ order by last_name;
 
 --Calculate the count of customers from Mexico and contact signed as ‘Owner’.
 select count(*) from customers 
-where country  like 'Mexico'
+where country like 'Mexico'
 and contact_title like 'Owner';
 
 
--- L4-SIMPLE_REPORTS.sql
+++ L4-SIMPLE_REPORTS.sql
@@ -1,3 +1,4 @@
+--Practice
 --Write a query to get most expense and least expensive Product list (name and unit price).
 select product_name
 , unit_price
@@ -23,12 +24,53 @@ inner join orders o on e.employee_id = o.employee_id
 group by o.employee_id, e.first_name, e.last_name;
 
 
+--Calculate the greatest, the smallest and the average age among the employees from London.
+select (SELECT min(date_part('year', CURRENT_DATE) - date_part('year', birth_date)) from employees) as min_age
+, (SELECT max(date_part('year', CURRENT_DATE) - date_part('year', birth_date)) from employees) as max_age
+, (SELECT ROUND(avg(date_part('year', CURRENT_DATE) - date_part('year', birth_date))) from employees) as avg_age;
 
+--Show the list of cities in which the average age of employees is greater than 60 (the average age is also to be shown)
+select city, avg(date_part('year', CURRENT_DATE) - date_part('year', birth_date)) as avg_age 
+from employees
+group by city having avg(date_part('year', CURRENT_DATE) - date_part('year', birth_date)) > 60;
 
+--Show the first and last name(s) of the eldest employee(s).
+select first_name || ' ' || last_name as full_name
+, date_part('year', CURRENT_DATE) - date_part('year', birth_date) as age
+from employees e 
+order by birth_date;
 
+--Show first, last names and ages of 3 eldest employees.
+select first_name || ' ' || last_name as full_name
+, date_part('year', CURRENT_DATE) - date_part('year', birth_date) as age
+from employees e 
+order by birth_date
+limit 3;
 
+--HOME WORK
+--Write a query to get Product name and quantity/unit.
+select product_name, quantity_per_unit from products;
 
+--4. Write a query to get most expense and least expensive Product list (name and unit price). 
+select product_name, unit_price from products
+where product_id in ((select product_id from products order by unit_price limit 1)
+, (select product_id from products order by unit_price desc limit 1));
 
+--5. Write a query to get Product list (id, name, unit price) where current products cost less than $20. 
+select product_id, product_name, unit_price from products
+where unit_price < 20;
+
+--8. Write a query to get Product list (name, unit price) of ten most expensive products. 
+select product_name, unit_price from products
+order by unit_price desc limit 10;
+
+--9. Write a query to count current and discontinued products. 
+select count(product_id), discontinued  from products
+group by discontinued;
+
+--10. Write a query to get Product list (name, units on order , units in stock) of stock is less than the quantity on order.
+select product_name, unit_price, units_in_stock  from products
+where units_in_stock < units_on_order;
 
 
 
