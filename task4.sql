0fd5e638a06c9b5ea415db526df668775da8a04a
maksym-shulha <123635020+maksym-shulha@users.noreply.github.com>
2023-05-17 14:44:38 +0300

    add solutions to task4

--- task4.sql
+++ task4.sql
@@ -119,3 +119,113 @@ select
 from employees
 group by title, title_of_courtesy
 -----------------------------------------------------------------
+
+--create a report with employees full names and their time on job (<30 years, 30 years, >30 years)
+
+select
+	concat_ws(' ', e.first_name, e.last_name) as full_name,
+	case
+		when extract(year from age(e.hire_date)) < 30 then 'less than 30 years'
+		when extract(year from age(e.hire_date)) = 30 then '30 years'
+		else 'more than 30 years'
+	end as years_on_job
+from employees e
+-----------------------------------------------------------------
+
+--Write a query to get Product name and quantity/unit.
+
+select
+	p.product_name
+	,p.quantity_per_unit
+from products p
+-----------------------------------------------------------------
+
+--Write a query to get current Product list (Product ID and name).
+
+select
+	p.product_id
+	,p.product_name
+from products p
+where p.discontinued <> 1
+-----------------------------------------------------------------
+
+--Write a query to get discontinued Product list (Product ID and name).
+
+select
+	p.product_id
+	,p.product_name
+from products p
+where p.discontinued = 1
+-----------------------------------------------------------------
+
+--Write a query to get most expense and least expensive Product list (name and unit price)
+
+select
+	p.product_name
+	,p.unit_price
+from products p
+where p.unit_price = (select max(unit_price) from products)
+	or p.unit_price = (select min(unit_price) from products)
+order by p.unit_price desc
+-----------------------------------------------------------------
+
+--Write a query to get Product list (id, name, unit price) where current products cost less than $20
+
+select
+	p.product_id
+	,p.product_name
+	,p.unit_price
+from products p
+where p.discontinued <> 1 and p.unit_price < 20
+-----------------------------------------------------------------
+
+--Write a query to get Product list (id, name, unit price) where products cost between $15 and $25
+
+select
+	p.product_id
+	,p.product_name
+	,p.unit_price
+from products p
+where p.unit_price between 15 and 25
+-----------------------------------------------------------------
+
+--Write a query to get Product list (name, unit price) of above average price.
+
+select
+	p.product_name
+	,p.unit_price
+from products p
+where p.unit_price > (select avg(unit_price) from products)
+-----------------------------------------------------------------
+
+--Write a query to get Product list (name, unit price) of ten most expensive products.
+
+select
+	p.product_name
+	,p.unit_price
+from products p
+order by p.unit_price desc
+limit 10
+-----------------------------------------------------------------
+
+--Write a query to count current and discontinued products.
+
+select
+	case
+		when p.discontinued = 1 then 'discontinued'
+		else 'current'
+	end as status
+	,count(*) as number_of_products
+from products p
+group by p.discontinued
+-----------------------------------------------------------------
+
+--Write a query to get Product list (name, units on order, units in stock) if stock is less than the quantity on order.
+
+select
+	p.product_name
+	,p.units_on_order
+	,p.units_in_stock
+from products p
+where p.units_in_stock < p.units_on_order
+-----------------------------------------------------------------
