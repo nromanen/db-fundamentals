03b3eccc7863e36d19d7ef6e0fb0d5b46999779d
GrigoriyKosarev <grigoriykosarev@gmail.com>
2023-05-19 11:33:34 +0300

    Update task4.sql

--- task4.sql
+++ task4.sql
@@ -1,4 +1,20 @@
--- 1) Task
+-- 1) Show the date of the first order ever made in the Orders table.
+      Note. There’s a aggregate function called Min that you need to use for this problem
 
-select
+select min(o.order_date)
+from orders o 
 ----------------------------------------------
+
+-- 2) Show a list of countries where the Northwind company has customers and the number of Customers who work there (alias NumberOfCustomers).
+      The result should be sorted by NumberOfCustomers in descending order
+      Note. You need to use grouping and an aggregate function called COUNT() for this problem.
+      
+select  
+	country,
+	COUNT(customer_id) as NumberOfCustomers 
+from customers c
+Group by 
+	country
+ORDER BY
+    NumberOfCustomers DESC,
+    country ASC 
