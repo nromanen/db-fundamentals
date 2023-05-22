f23b596585e4fb32e1f93af73ce4eaf8e7f4fac2
Oleksiy Rudeyko <oleksiy.rudeyko@gmail.com>
2023-05-22 10:26:03 +0300

    task 4

--- hw04.sql
+++ hw04.sql
@@ -96,4 +96,73 @@ from Employees
 group by Title, TitleOfCourtesy
 
 
---
+--create a report with employees full names and their time on job (<30 years, 30 years, >30 years)
+
+select concat_ws(' ', e.first_name, e.last_name) as full_name,
+case
+	when extract(year from age(e.hire_date)) < 30 then 'less than 30 years'
+	when extract(year from age(e.hire_date)) = 30 then '30 years'
+	else 'more than 30 years'
+end as years_on_job
+from employees e
+
+
+--Write a query to get Product name and quantity/unit.
+
+select p.product_name, p.quantity_per_unit
+from products p
+
+
+--Write a query to get current Product list (Product ID and name).
+
+select p.product_id, p.product_name
+from products p
+where p.discontinued <> 1
+
+
+--Write a query to get discontinued Product list (Product ID and name).
+
+select p.product_id, p.product_name
+from products p
+where p.discontinued = 1
+
+
+--Write a query to get Product list (id, name, unit price) where products cost between $15 and $25
+
+select p.product_id, p.product_name, p.unit_price
+from products p
+where p.unit_price between 15 and 25
+
+
+--Write a query to get Product list (name, unit price) of above average price.
+
+select p.product_name, p.unit_price
+from products p
+where p.unit_price > (select avg(unit_price) from products)
+
+
+--Write a query to get Product list (name, unit price) of ten most expensive products.
+
+select p.product_name, p.unit_price
+from products p
+order by p.unit_price desc
+limit 10
+
+
+--Write a query to count current and discontinued products.
+
+select
+case
+	when p.discontinued = 1 then 'discontinued'
+	else 'current'
+end as status
+	,count(*) as number_of_products
+from products p
+group by p.discontinued
+
+
+--Write a query to get Product list (name, units on order, units in stock) if stock is less than the quantity on order.
+
+select p.product_name, p.units_on_order, p.units_in_stock
+from products p
+where p.units_in_stock < p.units_on_order
